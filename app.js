9551603ca83d97fe13f9f7d3b0352b75cd313a66
nemo6 <miguel.maudet.contact@gmail.com>
2023-08-11 07:39:17 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,24 +3,24 @@ const path = require("path")
 
 /*function walk_folder(dir,table=[],level=0) {
 
-    let list = fs.readdirSync(dir)
-    
-    for ( let file of list ){
-    
-        let pathx = dir + '\\' + file
-        let stats = fs.statSync(pathx)
-        
-        if( stats.isFile() ){
-            table.push(pathx)
-        }
-        else if ( stats.isDirectory() ){
-            level++
-            walk_csv(pathx,table,level)
-        }
-    
-    }
+	let list = fs.readdirSync(dir)
+	
+	for ( let file of list ){
+	
+		let pathx = dir + '\\' + file
+		let stats = fs.statSync(pathx)
+		
+		if( stats.isFile() ){
+			table.push(pathx)
+		}
+		else if ( stats.isDirectory() ){
+			level++
+			walk_folder(pathx,table,level)
+		}
+	
+	}
 
-    return table
+	return table
 }*/
 
 function walk_folder(dir,obj={},i=0) {
